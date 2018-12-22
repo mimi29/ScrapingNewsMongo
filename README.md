154e9276801945c28fa53794f266a78127a171e7
unknown <ming29398@gmail.com>
2018-12-21 17:13:43 -0800

    initial checked in

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ScrapingNewsMongo
+
+1. scrape the web site (yes)
+2. insert to mongo db (yes) only scrape if count = 0
+3. work with express ui
---
+++ models/Article.js
@@ -0,0 +1,54 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+// This is similar to a Sequelize model
+var ArticleSchema = new Schema({
+  // `title` is required and of type String
+  title: {
+    type: String,
+    required: true,
+    validate: {
+      isAsync: true,
+      validator: function(value, isValid) {
+        const self = this;
+        return self.constructor.findOne({title: value})
+          .exec(function(err, article) {
+            if (err) {
+              console.log(err);
+            }
+            else if (article) {
+              if (self.id === article.id) {
+                return isValid(true);
+              }
+              return isValid(false);
+            }
+            else {
+              return isValid(true);
+            }
+          })
+      },
+      message: "Article is already existed in Database"
+    },
+  },
+  // `link` is required and of type String
+  link: {
+    type: String,
+    required: true
+  },
+  // `note` is an object that stores a Note id
+  // The ref property links the ObjectId to the Note model
+  // This allows us to populate the Article with an associated Note
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the Article model
+module.exports = Article;
